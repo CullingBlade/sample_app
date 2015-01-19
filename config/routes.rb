ac6cae4eb7efa11ae33d016a1e38e64aaedc3dfc
CullingBlade <nvluan.92@gmail.com>
2015-01-19 13:42:10 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root 'static_pages#home'
   get 'home' =>'static_pages/home'
@@ -7,6 +6,8 @@ Rails.application.routes.draw do
   get 'contact' =>  'static_pages#contact'
   get 'about' =>  'static_pages#about'
   get 'signup'  =>  'users#new'
+
+  resources :users
 
 
   # The priority is based upon order of creation: first created -> highest priority.
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com",
+    @user = User.new(name: "nvluan", email: "nvluan@gmail.com",
     				password: "foobar", password_confirmation: "foobar")
   end
 
@@ -33,7 +33,7 @@ class UserTest < ActiveSupport::TestCase
   	assert_not @user.valid?
   end
 
-test "email validation should accept valid addresses" do
+  test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
@@ -42,19 +42,25 @@ test "email validation should accept valid addresses" do
     end
   end
 
-test "email validation should reject invalid addresses" do
+  test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
-end
+  end
 
-test "email addresses should be unique" do
+  test "email addresses should be unique" do
 	duplicate_user = @user.dup
 	@user.save
 	assert_not duplicate_user.valid?
-end
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
 end
 
