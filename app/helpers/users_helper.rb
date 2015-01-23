566a5dab27596de4734dffdf5db6a2455c8df00a
CullingBlade <nvluan.92@gmail.com>
2015-01-23 09:06:20 +0700

    Chapter 9 Mission complete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,8 @@ module UsersHelper
   # Returns the Gravatar for the given user.
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "http://3.bp.blogspot.com/-_ESRdFrFsEg/UPAe6f1NuGI/AAAAAAAAd2M/Q8Bh7FgqzFc/s1600/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
+
